c1f4bcc394c5b9626fd2934130e5858c6aa62c8b
Scott van Looy <scott@quul.net>
2018-10-29 16:05:07 +0100

    Small changes to expose breakpoints to JS

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@
 
 'use strict';
 
+// Breakpoints
+var OLD_MOBILE = $$oldMobile$$; //eslint-disable-line
+var MOBILE = $$mobile$$; //eslint-disable-line
+var SMALL_TABLET = $$smalltablet$$; //eslint-disable-line
+var TABLET = $$tablet$$; //eslint-disable-line
+var SMALL_DESKTOP = $$smalldesktop$$; //eslint-disable-line
+
 console.log('foo');
