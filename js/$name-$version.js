62df14a51e7b01a2d7042146ff63afb804a0760e
Scott van Looy <scott@quul.net>
2018-11-06 07:31:31 +0100

    templating for js/css names

---
+++ js/$name-$version.js
@@ -0,0 +1,12 @@
+/*eslint-disable no-console*/
+
+'use strict';
+
+// Breakpoints
+var OLD_MOBILE = $$oldMobile$$; //eslint-disable-line
+var MOBILE = $$mobile$$; //eslint-disable-line
+var SMALL_TABLET = $$smalltablet$$; //eslint-disable-line
+var TABLET = $$tablet$$; //eslint-disable-line
+var SMALL_DESKTOP = $$smalldesktop$$; //eslint-disable-line
+
+console.log('main app');
---
+++ js/admin-$version.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*eslint-disable no-console*/
+
+// Breakpoints
+var OLD_MOBILE = $$oldMobile$$; //eslint-disable-line
+var MOBILE = $$mobile$$; //eslint-disable-line
+var SMALL_TABLET = $$smalltablet$$; //eslint-disable-line
+var TABLET = $$tablet$$; //eslint-disable-line
+var SMALL_DESKTOP = $$smalldesktop$$; //eslint-disable-line
+
+console.log('admin');
