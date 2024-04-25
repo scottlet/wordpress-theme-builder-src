df9fb5ab694d2bc2abc40056dbbd19ad9816f1fd
Scott van Looy <scott@quul.net>
2024-04-25 08:32:53 +0200

    New files

---
+++ .eslintrc.js
@@ -0,0 +1,130 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+
+  plugins: [
+    'jsdoc',
+    'import',
+    'prefer-smart-quotes',
+    'prettier',
+    'react',
+    'react-hooks'
+  ],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['~', './js/modules']]
+      }
+    },
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    '../.eslintrc.js',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2022
+  },
+
+  rules: {
+    'array-bracket-spacing': 1,
+    'arrow-parens': [2, 'as-needed'],
+    'block-scoped-var': 2,
+    'block-spacing': 2,
+    'brace-style': 2,
+    camelcase: 2,
+    'comma-dangle': 2,
+    'comma-spacing': [
+      1,
+      {
+        before: false,
+        after: true
+      }
+    ],
+    complexity: [1, 9],
+    'consistent-return': 2,
+    'dot-notation': 2,
+    'eol-last': 2,
+    'func-call-spacing': [2, 'never'],
+    'func-style': [2, 'declaration'],
+    indent: [2, 2],
+    'lines-around-directive': 2,
+    'max-len': [1, 120],
+    'new-cap': 0,
+    'newline-after-var': 2,
+    'newline-before-return': 2,
+    'no-alert': 1,
+    'no-console': 2,
+    'no-empty-function': 2,
+    'no-else-return': 2,
+    'no-eval': 2,
+    'no-extend-native': 2,
+    'no-extra-parens': 0,
+    'no-loop-func': 2,
+    'no-magic-numbers': [
+      2,
+      {
+        enforceConst: true,
+        ignoreArrayIndexes: true,
+        ignore: [
+          -1, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0, 1, 1.5, 2, 3, 4,
+          5, 6, 7, 8, 9, 10, 15, 16, 100, 250, 256, 500, 512, 1000
+        ]
+      }
+    ],
+    'no-multi-spaces': 2,
+    'no-multiple-empty-lines': 2,
+    'no-redeclare': 1,
+    'no-trailing-spaces': 2,
+    'no-underscore-dangle': 2,
+    'no-unused-expressions': 2,
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none'
+      }
+    ],
+    'no-useless-return': 2,
+    'object-curly-spacing': [2, 'always'],
+    'object-shorthand': 2,
+    'one-var': [2, 'never'],
+    'padding-line-between-statements': [
+      2,
+      { blankLine: 'always', prev: 'block-like', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'block-like' }
+    ],
+    'prefer-arrow-callback': 2,
+    'prefer-const': 2,
+    'no-var': 2,
+    quotes: [2, 'single', 'avoid-escape'],
+    radix: [2, 'as-needed'],
+    'react/react-in-jsx-scope': 0,
+    'space-infix-ops': 2,
+    'linebreak-style': [1, 'unix'],
+    semi: [2, 'always'],
+    'space-before-function-paren': [
+      2,
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'keyword-spacing': 2,
+    'space-before-blocks': 2,
+    strict: [2, 'global'],
+    'valid-jsdoc': 2,
+    'vars-on-top': 1,
+    'import/no-unresolved': [
+      2,
+      { commonjs: true, ignore: ['compiledHandlebars'] }
+    ]
+  }
+};
